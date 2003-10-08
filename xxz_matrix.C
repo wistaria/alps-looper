2ad8f6c38dfb05eb143d81e1ad165c7ba51705e0
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2003-10-09 00:22:51 +0900

    ------------------------------------------------------------------------ r394 | wistaria | 2003-10-09 00:22:51 +0900 (Thu, 09 Oct 2003) | 2 lines
    *** empty log message ***
    
    ------------------------------------------------------------------------

--- xxz_matrix.C
+++ xxz_matrix.C
@@ -14,14 +14,18 @@ int main()
 
     std::cout << "input parameters: S0 = " << s0 << ", S1 = " << s1
 	      << ", e0 = " << e0 << ", Jxy = " << jxy << ", Jz = " << jz
-	      << std::endl
-	      << xxz.matrix() << std::endl;
+	      << std::endl << xxz << std::endl;
 
-    boost::tuple<double, double, double> p =
-      looper::fit_xxz_matrix(s0, s1, xxz.matrix());
-    std::cout << "fitting result: e0 = " << p.get<0>()
-	      << ", Jxy = " << p.get<1>()
-	      << ", Jz = " << p.get<2>()
-	      << std::endl;
+    boost::tuple<bool, double, double, double> fit =
+      looper::fit_xxz_matrix(s0, s1, xxz);
+
+    if (fit.get<0>()) {
+      std::cout << "fitting result: e0 = " << fit.get<1>()
+		<< ", Jxy = " << fit.get<2>()
+		<< ", Jz = " << fit.get<3>()
+		<< std::endl;
+    } else {
+      std::cout << "fitting failed\n";
+    }
   }
 }
--- xxz_matrix.h
+++ xxz_matrix.h
@@ -3,8 +3,8 @@
 
 #include <alps/parameters.h>
 #include <alps/model.h>
-#include <boost/multi_array.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
 namespace looper {
@@ -15,7 +15,10 @@ class xxz_matrix
 public:
   typedef T value_type;
   typedef M matrix_type;
+  typedef typename matrix_type::size_type size_type;
 
+  xxz_matrix() : matrix_() {}
+  xxz_matrix(const xxz_matrix& m) : matrix_(m.matrix_) {}
   template<class I>
   xxz_matrix(const alps::half_integer<I>& s0, const alps::half_integer<I>& s1,
 	     double e0, double jxy, double jz) : matrix_()
@@ -25,16 +28,26 @@ public:
   matrix_type& matrix() { return matrix_; }
   const matrix_type& matrix() const { return matrix_; }
 
+  // access to to rows
+  typename matrix_type::matrix_row_type operator[](size_type i) {
+    return matrix_[i];
+  }
+  typename matrix_type::const_matrix_row_type operator[](size_type i) const {
+    return matrix_[i];
+  }
+
   template<class I>
   void build(const alps::half_integer<I>& s0, const alps::half_integer<I>& s1,
-	     double e0, double jz, double jxy)
+	     double e0, double jxy, double jz)
   {
     typedef alps::half_integer<I> half_integer_type;
-    const half_integer_type u(1);
     
     // set matrix dimension
-    int dim = (s0.get_twice() + 1) * (s1.get_twice() + 1);
+    int dim = (s0.get_twice()+1) * (s1.get_twice()+1);
     matrix_.resize(dim, dim);
+    for (int i = 0; i < dim; ++i)
+      for (int j = 0; j < dim; ++j)
+	matrix_[i][j] = value_type(0);
 
     // diagonal elements: jz sz0 sz1
     for (half_integer_type sz0 = s0; sz0 >= -s0; --sz0) {
@@ -45,49 +58,94 @@ public:
     }
 
     // off-diagonal elements: jxy s0+ s1- / 2
-    for (half_integer_type sz0 = s0 - 1; sz0 >= -s0; --sz0) {
-      for (half_integer_type sz1 = s1; sz1 >= -s1 + 1; --sz1) {
-	matrix_[index(s0, s1, sz0 + 1, sz1 - 1)][index(s0, s1, sz0, sz1)] =
+    for (half_integer_type sz0 = s0-1; sz0 >= -s0; --sz0) {
+      for (half_integer_type sz1 = s1; sz1 >= -s1+1; --sz1) {
+	matrix_[index(s0, s1, sz0+1, sz1-1)][index(s0, s1, sz0, sz1)] =
 	  - 0.5 * jxy *
-	  std::sqrt(double(s0 - sz0) * double(s0 + sz0 + 1)) * 
-	  std::sqrt(double(s1 + sz1) * double(s1 - sz1 + 1));
+	  std::sqrt(double(s0-sz0) * double(s0+sz0+1)) * 
+	  std::sqrt(double(s1+sz1) * double(s1-sz1+1));
       }
     }
 
     // off-diagonal elements: jxy s0- s1+ / 2
-    for (half_integer_type sz0 = s0; sz0 >= -s0 + 1; --sz0) {
-      for (half_integer_type sz1 = s1 - 1; sz1 >= -s1; --sz1) {
-	matrix_[index(s0, s1, sz0 - 1, sz1 + 1)][index(s0, s1, sz0, sz1)] =
+    for (half_integer_type sz0 = s0; sz0 >= -s0+1; --sz0) {
+      for (half_integer_type sz1 = s1-1; sz1 >= -s1; --sz1) {
+	matrix_[index(s0, s1, sz0-1, sz1+1)][index(s0, s1, sz0, sz1)] =
 	  - 0.5 * jxy *
-	  std::sqrt(double(s0 + sz0) * double(s0 - sz0 + 1)) * 
-	  std::sqrt(double(s1 - sz1) * double(s1 + sz1 + 1));
+	  std::sqrt(double(s0+sz0) * double(s0-sz0+1)) * 
+	  std::sqrt(double(s1-sz1) * double(s1+sz1+1));
       }
     }
   }
   
 protected:
-  xxz_matrix();
-
   template<class I>
   I index(const alps::half_integer<I>& s0,
 	  const alps::half_integer<I>& s1,
 	  const alps::half_integer<I>& sz0,
 	  const alps::half_integer<I>& sz1) const
   {
-    return s0.distance(sz0) * (s1.get_twice() + 1) + s1.distance(sz1);
+    return s0.distance(sz0) * (s1.get_twice()+1) + s1.distance(sz1);
   }
 
 private:
   matrix_type matrix_;
 };
 
+template <class T, class M>
+inline std::ostream& operator<<(std::ostream& os, const xxz_matrix<T, M>& m)
+{
+  os << m.matrix();
+  return os;
+}
+
+//
+// fit a matrix to xxz_matrix
+//
+
 template <class I, class M>
-inline boost::tuple<typename M::value_type, M::value_type, M::value_type>
+inline boost::tuple<bool, typename M::value_type, M::value_type, M::value_type>
 fit_xxz_matrix(const alps::half_integer<I>& s0, 
 	       const alps::half_integer<I>& s1, 
-	       const M& m)
+	       const M& mat, typename M::value_type tol = 1.0e-10)
 {
-  return boost::make_tuple(0, 0, 0);
+  typedef M matrix_type;
+  typedef typename M::value_type value_type;
+  typedef boost::numeric::ublas::scalar_vector<value_type> vector_type;
+
+  int dim = (s0.get_twice()+1) * (s1.get_twice()+1);
+  xxz_matrix<value_type> m1(s0, s1, 0, 1, 1);
+
+  // e0
+  value_type e0 = 0;
+  for (int i = 0; i < dim; ++i) e0 += mat[i][i];
+  e0 /= dim;
+
+  // jz
+  value_type jz = (mat[0][0] - e0) / m1[0][0];
+
+  // jxy
+  double jxy = 0;
+  for (int i = 0; i < dim; ++i) {
+    for (int j = 0; j < dim; ++j) {
+      if ((i != j) && (m1[i][j] != 0)) {
+	jxy = mat[i][j] / m1[i][j];
+	break;
+      }
+    }
+    if (jxy != 0) break;
+  }
+
+  // check
+  bool success = true;
+  xxz_matrix<value_type> m(s0, s1, e0, jxy, jz);
+  for (int i = 0; i < dim; ++i) {
+    for (int j = 0; j < dim; ++j) {
+      if (std::abs(mat[i][j] - m[i][j]) > tol) success = false;
+    }
+  }
+
+  return boost::make_tuple(success, e0, jxy, jz);
 }
 
 } // end namespace looper
