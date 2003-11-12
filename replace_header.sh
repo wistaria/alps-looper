16e55968824cdd020008bb2c27446d9558d142aa
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2003-11-12 11:58:33 +0900

    ------------------------------------------------------------------------ r555 | wistaria | 2003-11-12 11:58:33 +0900 (Wed, 12 Nov 2003) | 2 lines
    *** empty log message ***
    
    ------------------------------------------------------------------------

--- replace_header.sh
+++ replace_header.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 find . -name Makefile.in | xargs ./replace_header.pl
+find . -name '*.h.in' | xargs ./replace_header.pl
 find . -name '*.h' | xargs ./replace_header.pl
+find . -name '*.C.in' | xargs ./replace_header.pl
 find . -name '*.C' | xargs ./replace_header.pl
+
+find . -name '*.orig' | xargs rm
