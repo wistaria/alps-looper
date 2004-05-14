9cf9952f31d6e38952199daa2bef595dcbd1a143
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2004-05-14 19:52:03 +0900

    ------------------------------------------------------------------------ r802 | wistaria | 2004-05-14 19:52:03 +0900 (Fri, 14 May 2004) | 2 lines
    *** empty log message ***
    
    ------------------------------------------------------------------------

--- touch_script.sh
+++ touch_script.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-dir=`dirname $0`
-
-touch $dir/aclocal.m4
-find $dir -name Makefile.in | xargs touch
-touch $dir/configure
-find $dir -name config.h.in | xargs touch
+for p in aclocal automake autoconf
+do
+  ./missing $p
+done
