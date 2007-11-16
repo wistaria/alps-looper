819c34149f8cdee592d3a39ca6d3994837b4e654
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2007-11-16 12:06:27 +0900

    ------------------------------------------------------------------------ r2620 | wistaria | 2007-11-16 12:06:27 +0900 (Fri, 16 Nov 2007) | 1 line
    ------------------------------------------------------------------------

--- config/extract_version.sh
+++ config/extract_version.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# for echo without trailing newline character
+case `echo "testing\c"; echo 1,2,3`,`echo -n testing; echo 1,2,3` in
+  *c*,-n*) ECHO_N= ECHO_C='
+' ;;
+  *c*,*  ) ECHO_N=-n ECHO_C= ;;
+  *)       ECHO_N= ECHO_C='\c' ;;
+esac
+
 basedir=`dirname $0`
-/bin/echo -n `awk '$2=="LOOPER_VERSION" {print $3}' $basedir/../looper/version.h | sed s/\"//g`
+echo $ECHO_N `awk '$2=="LOOPER_VERSION" {print $3}' $basedir/../looper/version.h | sed s/\"//g`$ECHO_C
