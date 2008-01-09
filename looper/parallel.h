8f9596082affa2310549e26beddd82d00bdf23f8
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2008-01-10 00:47:34 +0900

    ------------------------------------------------------------------------ r2697 | wistaria | 2008-01-10 00:47:34 +0900 (Thu, 10 Jan 2008) | 1 line
    ------------------------------------------------------------------------

--- looper/parallel.h
+++ looper/parallel.h
@@ -43,7 +43,7 @@ public:
   class flip_t {
   public:
     flip_t() : flip_(id_mask) {}
-    void set_flip(bool flip) { flip_ = (flip ? 1 : 0); }
+    void set_flip(int flip) { flip_ = flip; }
     bool flip() const { return flip_ == 1; }
     bool open() const { return flip_ < 0; }
     void set_id(int id) { flip_ = id ^ id_mask; }
@@ -53,21 +53,39 @@ public:
     int flip_; // negative if cluster is still open
   };
 
+  struct info_t {
+    accumulate_t accum;
+    int nc;
+  };
+
   parallel_cluster_unifier(MPI_Comm comm, int num_sites) :
-    comm_(comm), num_sites_(num_sites), num_boundaries_(2 * num_sites),
-    flip_(num_boundaries_), flip_close_(num_boundaries_), flip_stage_(num_boundaries_),
-    links_(2 * num_boundaries_), linksD_(num_boundaries_), linksU_(num_boundaries_),
-    estimates_(2 * num_boundaries_), estimatesD_(num_boundaries_), estimatesU_(num_boundaries_) {
+    comm_(comm), num_sites_(num_sites), num_boundaries_(2 * num_sites) {
 
     int info;
     if ((info = MPI_Comm_size(comm_, &num_processes_)) != 0)
       boost::throw_exception(std::runtime_error(("Error " +
         boost::lexical_cast<std::string>(info) + " in MPI_Comm_size")));
-    if ((info = MPI_Comm_rank(comm, &process_id_)) != 0)
-      boost::throw_exception(std::runtime_error(("Error " +
-        boost::lexical_cast<std::string>(info) + " in MPI_Comm_rank")));
-    num_stages_ = 0;
-    for (int t = (num_processes_ - 1); t > 0; t = (t >> 1)) ++num_stages_;
+
+    if (num_processes_ == 1) {
+      links_.resize(num_boundaries_);
+      linksD_.resize(num_boundaries_);
+      estimates_.resize(num_boundaries_);
+      estimatesD_.resize(num_boundaries_);
+    } else {
+      if ((info = MPI_Comm_rank(comm, &process_id_)) != 0)
+        boost::throw_exception(std::runtime_error(("Error " +
+          boost::lexical_cast<std::string>(info) + " in MPI_Comm_rank")));
+      num_stages_ = 0;
+      for (int t = (num_processes_ - 1); t > 0; t = (t >> 1)) ++num_stages_;
+
+      flip_stage_.resize(num_boundaries_);
+      links_.resize(2 * num_boundaries_);
+      linksD_.resize(num_boundaries_);
+      linksU_.resize(num_boundaries_);
+      estimates_.resize(2 * num_boundaries_);
+      estimatesD_.resize(num_boundaries_);
+      estimatesU_.resize(num_boundaries_);
+    }
   }
 
   template<typename FRAGMENT, typename RNG>
@@ -75,6 +93,9 @@ public:
     std::vector<estimate_t> const& estimates, accumulate_t& accum, RNG& r_uniform) {
 
     // initialize local tables
+    info_.nc = num_clusters;
+    info_.accum = accum;
+    flip_.resize(num_clusters);
     for (int c = 0; c < num_clusters; ++c) {
       flip_[c].set_id(c);
       estimatesD_[c] = estimates[c];
@@ -86,112 +107,155 @@ public:
         linksD_[v].set_parent(root_index(fragments, v));
     }
 
-    MPI_Status status;
-    for (int stage = 0; stage < num_stages_; ++stage) {
-      const int stage_mask = (1 << (stage + 1)) - 1; // 000011111 for stage = 4
-      const int target_mask = (1 << stage);          // 000010000
+    if (num_processes_ == 1) {
 
-      if ((process_id_ & stage_mask) == 0 &&
-          process_id_ + target_mask < num_processes_) {
-        // master process
-        const int slave = process_id_ + target_mask;
+      for (int v = 0; v < num_boundaries_; ++v) links_[v] = linksD_[v];
+      for (int v = 0; v < num_sites_; ++v) looper::union_find::unify(links_, v, num_sites_ + v);
 
-        // construct link table
-        for (int v = 0; v < num_boundaries_; ++v) {
-          if (linksD_[v].is_root()) {
-            links_[v] = link_t();
-          } else {
-            links_[v].set_parent(linksD_[v].parent());
-          }
-        }
-        accumulate_t accumU;
-        MPI_Recv(&accumU, sizeof(accumulate_t), MPI_BYTE, slave, stage, comm_, &status);
-        accum.add(accumU);
-        MPI_Recv(&linksU_[0], num_boundaries_, MPI_INT, slave, stage, comm_, &status);
-        for (int v = 0; v < num_boundaries_; ++v) {
-          if (linksU_[v].is_root()) {
-            links_[num_boundaries_ + v] = link_t();
-          } else {
-            links_[num_boundaries_ + v].set_parent(num_boundaries_ + linksU_[v].parent());
-          }
-        }
+      // assign cluster ID
+      int nc = 0; // total number of clusters
+      for (int v = 0; v < num_boundaries_; ++v) if (links_[v].is_root()) links_[v].set_id(nc++);
 
-        // connect between lower and upper
-        for (int v = 0; v < num_sites_; ++v)
-          looper::union_find::unify(links_, num_sites_ + v, num_boundaries_ + v);
+      // determine whether close clusters are flipped or not
+      for (int c = 0; c < num_clusters; ++c) flip_[c].set_flip(r_uniform() < 0.5);
+      std::fill(estimates_.begin(), estimates_.begin() + num_clusters, estimate_t());
 
-        // at final stage bottom and top must be connected too
-        if (stage + 1 == num_stages_)
-          for (int v = 0; v < num_sites_; ++v)
-            looper::union_find::unify(links_, v, num_boundaries_ + num_sites_ + v);
-        for (int v = 2 * num_boundaries_ - 1; v >= num_boundaries_ + num_sites_; --v)
-          set_root(links_, v);
-        for (int v = num_sites_ - 1; v >= 0; --v)
-          set_root(links_, v);
-
-        // assign cluster ID
-        int nc = 0; // total number of clusters
-        for (int v = 0; v < num_sites_; ++v)
-          if (links_[v].is_root()) links_[v].set_id(nc++);
-        for (int v = num_boundaries_ + num_sites_; v < 2 * num_boundaries_; ++v)
-          if (links_[v].is_root()) links_[v].set_id(nc++);
-        int noc = nc; // number of open clusters
-        for (int v = num_sites_; v < num_boundaries_ + num_sites_; ++v)
-          if (links_[v].is_root()) links_[v].set_id(nc++);
-        if (stage + 1 == num_stages_) noc = 0; // at finil stage there's no open clusters
-
-        // determine whether close clusters are flipped or not
-        for (int c = noc; c < nc; ++c) flip_close_[c] = (r_uniform() < 0.5);
-
-        // construct and send flip table for upper part
-        for (int v = 0; v < num_boundaries_; ++v) {
-          if (linksU_[v].is_root()) {
-            const int new_id = cluster_id(links_, num_boundaries_ + v);
-            if (new_id < noc)
-              flip_stage_[linksU_[v].id()].set_id(new_id);
-            else
-              flip_stage_[linksU_[v].id()].set_flip(flip_close_[new_id]);
-          }
-        }
-        MPI_Send(&flip_stage_[0], num_boundaries_, MPI_INT, slave, stage, comm_);
-
-        // construct flip table for lower part
-        for (int v = 0; v < num_boundaries_; ++v) {
-          if (linksD_[v].is_root()) {
-            const int new_id = cluster_id(links_, v);
-            if (new_id < noc)
-              flip_stage_[linksD_[v].id()].set_id(new_id);
-            else
-              flip_stage_[linksD_[v].id()].set_flip(flip_close_[new_id]);
-          }
-        }
-      } else if ((process_id_ & stage_mask) == target_mask) {
-        // slave process
-        const int master = process_id_ - target_mask;
-        MPI_Send(&accum, sizeof(accumulate_t), MPI_BYTE, master, stage, comm_);
-        MPI_Send(&linksD_[0], num_boundaries_, MPI_INT, master, stage, comm_);
-        MPI_Recv(&flip_stage_[0], num_boundaries_, MPI_INT, master, stage, comm_, &status);
-      } else {
-        // nothing to do
+      // construct flip table
+      for (int v = 0; v < num_boundaries_; ++v) {
+        const int new_id = cluster_id(linksD_, v);
+        const int old_id = cluster_id(links_, v);
+        if (linksD_[v].is_root()) flip_[old_id].set_flip(r_uniform() < 0.5);
+        estimates_[new_id] += estimatesD_[old_id];
       }
 
-      // distribute flip table to decendants
-      for (int s = stage - 1; s >= 0; --s) {
-        const int sm = (1 << (s + 1)) - 1;
-        const int tm = (1 << s);
-        if ((process_id_ & sm) == 0 && process_id_ + tm < num_processes_) {
-          const int slave = process_id_ + tm;
-          MPI_Send(&flip_stage_[0], num_boundaries_, MPI_INT, slave, s , comm_);
-        } else if ((process_id_ & sm) == tm) {
-          const int master = process_id_ - tm;
-          MPI_Recv(&flip_stage_[0], num_boundaries_, MPI_INT, master, s, comm_, &status);
+      // accumulate measurements of closed clusters
+      for (int c = 0; c < num_clusters; ++c) accum += estimates_[c];
+
+    } else {
+
+      MPI_Status status;
+      for (int stage = 0; stage < num_stages_; ++stage) {
+        const int stage_mask = (1 << (stage + 1)) - 1; // 000011111 for stage = 4
+        const int target_mask = (1 << stage);          // 000010000
+
+        if ((process_id_ & stage_mask) == 0 &&
+            process_id_ + target_mask < num_processes_) {
+          // master process
+          const int slave = process_id_ + target_mask;
+
+          // construct link table
+          for (int v = 0; v < num_boundaries_; ++v) {
+            if (linksD_[v].is_root()) {
+              links_[v] = link_t();
+            } else {
+              links_[v].set_parent(linksD_[v].parent());
+            }
+          }
+          MPI_Recv(&info_, sizeof(info_t), MPI_BYTE, slave, stage, comm_, &status);
+          accum += info_.accum;
+          MPI_Recv(&linksU_[0], num_boundaries_, MPI_INT, slave, stage, comm_, &status);
+          MPI_Recv(&estimatesU_[0], info_.nc * sizeof(estimate_t), MPI_BYTE, slave, stage, comm_,
+                   &status);
+          for (int v = 0; v < num_boundaries_; ++v) {
+            if (linksU_[v].is_root()) {
+              links_[num_boundaries_ + v] = link_t();
+            } else {
+              links_[num_boundaries_ + v].set_parent(num_boundaries_ + linksU_[v].parent());
+            }
+          }
+
+          // connect between lower and upper
+          for (int v = 0; v < num_sites_; ++v)
+            looper::union_find::unify(links_, num_sites_ + v, num_boundaries_ + v);
+
+          // at final stage bottom and top must be connected too
+          if (stage + 1 == num_stages_) {
+            for (int v = 0; v < num_sites_; ++v)
+              looper::union_find::unify(links_, v, num_boundaries_ + num_sites_ + v);
+          } else {
+            for (int v = 0; v < num_sites_; ++v) set_root(links_, v);
+            for (int v = num_boundaries_ + num_sites_; v < 2 * num_boundaries_; ++v)
+              set_root(links_, v);
+          }
+
+          // assign cluster ID
+          int nc = 0; // total number of clusters
+          for (int v = 0; v < num_sites_; ++v)
+            if (links_[v].is_root()) links_[v].set_id(nc++);
+          for (int v = num_boundaries_ + num_sites_; v < 2 * num_boundaries_; ++v)
+            if (links_[v].is_root()) links_[v].set_id(nc++);
+          int noc = nc; // number of open clusters
+          for (int v = num_sites_; v < num_boundaries_ + num_sites_; ++v)
+            if (links_[v].is_root()) links_[v].set_id(nc++);
+          if (stage + 1 == num_stages_) noc = 0; // at finil stage there's no open clusters
+
+          // determine whether close clusters are flipped or not
+          flip_close_.resize(nc);
+          for (int c = noc; c < nc; ++c) flip_close_[c] = (r_uniform() < 0.5);
+          std::fill(estimates_.begin(), estimates_.begin() + nc, estimate_t());
+
+          // construct and send flip table for upper part
+          for (int v = 0; v < num_boundaries_; ++v) {
+            const int new_id = cluster_id(links_, num_boundaries_ + v);
+            const int old_id = cluster_id(linksU_, v);
+            if (linksU_[v].is_root()) {
+              if (new_id < noc)
+                flip_stage_[old_id].set_id(new_id);
+              else
+                flip_stage_[old_id].set_flip(flip_close_[new_id]);
+            }
+            estimates_[new_id] += estimatesU_[old_id];
+          }
+          MPI_Send(&flip_stage_[0], num_boundaries_, MPI_INT, slave, stage, comm_);
+
+          // construct flip table for lower part
+          for (int v = 0; v < num_boundaries_; ++v) {
+            const int new_id = cluster_id(links_, v);
+            const int old_id = cluster_id(linksD_, v);
+            if (linksD_[v].is_root()) {
+              if (new_id < noc)
+                flip_stage_[old_id].set_id(new_id);
+              else
+                flip_stage_[old_id].set_flip(flip_close_[new_id]);
+            }
+            estimates_[new_id] += estimatesD_[old_id];
+          }
+
+          // accumulate measurements of closed clusters
+          for (int c = noc; c < nc; ++c) accum += estimates_[c];
+          info_.nc = noc;
+          info_.accum = accum;
+
+        } else if ((process_id_ & stage_mask) == target_mask) {
+          // slave process
+          const int master = process_id_ - target_mask;
+          MPI_Send(&info_, sizeof(info_t), MPI_BYTE, master, stage, comm_);
+          MPI_Send(&linksD_[0], num_boundaries_, MPI_INT, master, stage, comm_);
+          MPI_Send(&estimatesD_[0], info_.nc * sizeof(estimate_t), MPI_BYTE, master, stage, comm_);
+          MPI_Recv(&flip_stage_[0], num_boundaries_, MPI_INT, master, stage, comm_, &status);
         } else {
           // nothing to do
         }
+
+        // distribute flip table to decendants
+        for (int s = stage - 1; s >= 0; --s) {
+          const int sm = (1 << (s + 1)) - 1;
+          const int tm = (1 << s);
+          if ((process_id_ & sm) == 0 && process_id_ + tm < num_processes_) {
+            const int slave = process_id_ + tm;
+            MPI_Send(&flip_stage_[0], num_boundaries_, MPI_INT, slave, s , comm_);
+          } else if ((process_id_ & sm) == tm) {
+            const int master = process_id_ - tm;
+            MPI_Recv(&flip_stage_[0], num_boundaries_, MPI_INT, master, s, comm_, &status);
+          } else {
+            // nothing to do
+          }
+        }
+        // update flip table
+        for (int c = 0; c < num_clusters; ++c)
+          if (flip_[c].open()) flip_[c] = flip_stage_[flip_[c].id()];
       }
-      // update flip table
-      for (int c = 0; c < num_clusters; ++c)
-        if (flip_[c].open()) flip_[c] = flip_stage_[flip_[c].id()];
+
     }
 
     return flip_;
@@ -211,7 +275,8 @@ private:
   std::vector<flip_t> flip_;
 
   // working areas
-  std::vector<bool> flip_close_;
+  info_t info_;
+  std::vector<int> flip_close_;
   std::vector<flip_t> flip_stage_;
   std::vector<link_t> links_;
   std::vector<link_t> linksD_;
