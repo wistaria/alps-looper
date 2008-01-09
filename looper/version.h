1eb9c9232e30bb2b7b030ed18957a8b4e251233f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2008-01-09 10:51:02 +0900

    ------------------------------------------------------------------------ r2694 | wistaria | 2008-01-09 10:51:02 +0900 (Wed, 09 Jan 2008) | 1 line
    ------------------------------------------------------------------------

--- looper/version.h
+++ looper/version.h
@@ -28,10 +28,10 @@
 #include <iostream>
 
 /* Define the version of ALPS/looper */
-#define LOOPER_VERSION "3.2b5"
+#define LOOPER_VERSION "3.2b6-20080109"
 
 /* Define the published date of ALPS/looper */
-#define LOOPER_DATE "2008/01/08"
+#define LOOPER_DATE "2008/01/09"
 
 #include <alps/copyright.h>
 #include <iostream>
