d7fa3fcf13f4169cbcdeb1725a9d5de3e9154d2e
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2009-12-02 17:01:17 +0900

    ------------------------------------------------------------------------ r3467 | wistaria | 2009-12-02 17:01:17 +0900 (Wed, 02 Dec 2009) | 2 lines
    Fixed pre-compiled libboost_mpi detection for ticket #74
    
    ------------------------------------------------------------------------

--- looper/version.h
+++ looper/version.h
@@ -28,10 +28,10 @@
 #include <iostream>
 
 /* Define the version of ALPS/looper */
-#define LOOPER_VERSION "3.2b12-20091125"
+#define LOOPER_VERSION "3.2b12-20091202"
 
 /* Define the published date of ALPS/looper */
-#define LOOPER_DATE "2009/11/25"
+#define LOOPER_DATE "2009/12/02"
 
 #define LOOPER_VERSION_STRING "ALPS/looper version " LOOPER_VERSION " (" LOOPER_DATE ")"
 
