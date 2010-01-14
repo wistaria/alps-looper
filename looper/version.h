02a4027a095abe1ceff17e8002a9e589a89bceb2
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2010-01-14 12:15:06 +0900

    ------------------------------------------------------------------------ r3697 | wistaria | 2010-01-14 12:15:06 +0900 (Thu, 14 Jan 2010) | 1 line
    ------------------------------------------------------------------------

--- looper/version.h
+++ looper/version.h
@@ -28,10 +28,10 @@
 #include <iostream>
 
 /* Define the version of ALPS/looper */
-#define LOOPER_VERSION "3.2b12-20100107"
+#define LOOPER_VERSION "3.2b12-20100114"
 
 /* Define the published date of ALPS/looper */
-#define LOOPER_DATE "2010/01/07"
+#define LOOPER_DATE "2010/01/14"
 
 #define LOOPER_VERSION_STRING "ALPS/looper version " LOOPER_VERSION " (" LOOPER_DATE ")"
 
